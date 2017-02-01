a6ea656439c680987e14fe553865a23b87a4b7b8
Chanoniiz <chanon_darkside@hotmail.com>
2017-02-01 11:20:56 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -10,15 +10,15 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'ดี') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['ดี'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'งัว',
+				'type' => 'text',
 				'text' => $text
 			];
 
