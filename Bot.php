6b9eeffe00d2197672e5f8f860f4129fa26c0cbe
Chanoniiz <chanon_darkside@hotmail.com>
2017-02-01 11:30:33 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -17,16 +17,11 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			
 			$messages = [
-				
-				 "type":"text",
-				    "text":"Hello, user"
-				},
-				{
-				    "type":"text",
-				    "text":"May I help you?"
-				}
-						];
+				'type' => 'text',
+				'text' => $text
+			];	
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
