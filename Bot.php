a00dc7a24d5f32e46540873f0a797e5cf7cf6255
Chanoniiz <chanon_darkside@hotmail.com>
2017-02-06 15:21:51 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -1,6 +1,5 @@
-<script type="text/javascript" src="TestMqtt.php"></script>
-
 <?php
+include 'TestMqtt.php';
 $access_token = 'I10FeTUvizhulrxBQp4WTsRurtLiAvaLaASuEbK9/YU/zGV9BcPDMrrn28QBTD6Wh4ZL0BFSBQKTli+zaxY7zEdBb3u1xRu+6l8i9qt8leRllyTHUO+tNsXDz/Enq0DuC2muj7WvcseFkGTPFFTJdAdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
