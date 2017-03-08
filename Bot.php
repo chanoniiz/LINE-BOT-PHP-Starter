9369b3938951794fef511b97f3847a85fe4e17cd
Chanoniiz <chanon_darkside@hotmail.com>
2017-03-08 13:49:49 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -11,7 +11,7 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			$jsondata = "";
+			//$jsondata = "";
 			//var_dump(json_decode($events, true));
 			//$jsondata = var_dump(($events, true));
 			//$text = $event['message']['text'];
