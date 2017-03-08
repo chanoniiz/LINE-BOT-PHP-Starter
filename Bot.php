c3408ef6990a8b5ee797c7ef37156338c6834798
Chanoniiz <chanon_darkside@hotmail.com>
2017-03-08 12:59:11 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -6,12 +6,13 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
+$jsondata = "";
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
+			$jsondata = var_dump($events);
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
@@ -19,7 +20,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $jsondata
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
