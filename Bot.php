787556d9da8d14f363322d43b204440b5c6aa5b9
Chanoniiz <chanon_darkside@hotmail.com>
2017-05-25 20:36:34 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $data_json
+				'text' => $data_json.text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
