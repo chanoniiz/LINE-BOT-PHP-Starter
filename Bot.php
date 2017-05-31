6b0474a5bb9bb87a59461046d1e60a0328817b39
Chanoniiz <chanon_darkside@hotmail.com>
2017-05-31 20:49:26 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -22,6 +22,8 @@ if (!is_null($events['events'])) {
 			$myObj->threadID = $event['source']['userId'];
 			$myObj->form = "line";
 			
+			$threadID = $event['source']['userId'];
+			
 			$data_json = json_encode($myObj);
 			
 			//set jsondata to http  server 
@@ -29,7 +31,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $data_json
+				'text' => $threadID
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
