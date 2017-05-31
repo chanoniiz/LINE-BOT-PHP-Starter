b8d1a2e70c57154f47082ce9722e1fa8ca97bac5
Chanoniiz <chanon_darkside@hotmail.com>
2017-05-31 20:30:41 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -25,9 +25,9 @@ if (!is_null($events['events'])) {
 			$threadID = $event['userId'];
 		
 			
-			$data_json = $threadID ;
+			//$data_json = $threadID ;
 			
-			//$data_json = json_encode($myObj);
+			$data_json = json_encode($myObj);
 			
 			//set jsondata to http  server 
 			$jsondata = file_get_contents('http://202.28.37.32/smartcsmju/project_class/LineAPI/Bot.php?msg='.$data_json);
